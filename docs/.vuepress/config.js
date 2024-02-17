3bd2e4a6c6206b0418366dc9a460fa76ae2beb8f
open17 <opensci@163.com>
2024-02-17 14:07:46 +0800

    ✨ feat: PWA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,9 +21,25 @@ module.exports = {
                     "open17, 博客, vuepress, vuepress-theme-qbook, vuepress-theme, 主题, IT",
             },
         ],
-        ['meta', { name: 'baidu-site-verification', content: 'codeva-x3tdSgw9mt' }]
+        ['meta', { name: 'baidu-site-verification', content: 'codeva-x3tdSgw9mt' }],
+        // ['link', { rel: 'icon', href: 'favicon.ico', type: 'image/x-icon' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#fff' }],
+        // ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        // ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        // ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+        // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+        // ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+        // ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
     plugins: [
+        [
+            '@vuepress/pwa',
+            {
+                serviceWorker: true,
+                updatePopup: true
+            }
+        ]
     ],
     themeConfig: {
         avatar: "https://cdn.jsdelivr.net/gh/open17/Pic/img/202402031524062.webp",
