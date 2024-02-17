efac7814582c69a9113ac82c260bf150181bcbc1
open17 <opensci@163.com>
2024-02-17 15:42:24 +0800

    🐞 fix: 尝试修复pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,12 +25,12 @@ module.exports = {
         // ['link', { rel: 'icon', href: 'favicon.ico', type: 'image/x-icon' }],
         ['link', { rel: 'manifest', href: '/manifest.json' }],
         ['meta', { name: 'theme-color', content: '#fff' }],
-        // ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        // ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
         // ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
         // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
         // ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-        // ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
     plugins: [
         [
